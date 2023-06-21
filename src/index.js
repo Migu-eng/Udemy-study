af7abb634535c80d9f0dd591d94f292ed1c1f649
Migu-eng <134995420+Migu-eng@users.noreply.github.com>
2023-06-21 18:57:23 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+/**
+ * const,let等の変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// val1 = "var変数上書き";
+// console.log(val1);
+
+// var val1 = "var再上書き";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 ="let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// val3 ="constを上書き"
+
+// const val3 = "const変数を再宣言"
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "migu",
+//   age: 28
+// };
+// console.log(val4);
+// val4.name = "konnbu";
+// val4.address = "isikawa";
+// console.log(val4);
+
+//constで定義した配列はプロパテぃの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "cat";
+val5.push("monkey");
+console.log(val5);
