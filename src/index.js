3479f13404f5870cd4063304fa8a16282b264ed8
Migu-eng <134995420+Migu-eng@users.noreply.github.com>
2023-06-21 20:43:47 +0900

    デフォルト値、引数

--- src/index.js
+++ src/index.js
@@ -93,12 +93,16 @@
 // const message2 = `名前は${name}です。年齢は${age}歳ですよ。`;
 // console.log(message2);
 
-const myProfile = ["Migu", 23];
+// const myProfile = ["Migu", 23];
 
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-//配列分割代入
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// //配列分割代入
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+//デフォルト値,引数など
+const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+sayHello("Migu");
